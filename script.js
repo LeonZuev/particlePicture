a49a6e393f5f03765f854f5f7fa0bc7575542b92
leonzuev <loonatune@gmail.com>
2023-06-17 01:11:38 +0300

    Add particle draw method

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     const canvas = this.document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    // console.log(ctx);
-    canvas.width = this.window.innerWidth;
-    canvas.height = this.window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
     const image1 = this.document.getElementById('image1')
 
     class Particle {
-        constructor(){
-            this.x = 0;
-            this.y = 0;
-            this.size = 3;
+        constructor() {
+            this.x = 200;
+            this.y = 50;
+            this.size = 100;
         }
-
+        draw() {
+            ctx.fillRect(this.x, this.y, this.size, this.size);
+        }
     }
 
     class Effect {
 
     }
+    const particle1 = new Particle();
+    particle1.draw();
 
     function animate() {
 
     }
-    ctx.fillRect(120, 150, 100, 200)
-    ctx.drawImage(image1, 100, 100, 400, 300)
+
 });
