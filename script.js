876b874ce71fce7a2553ae452771b5d938e4e40f
leonzuev <loonatune@gmail.com>
2023-06-17 01:43:43 +0300

    Fix bugs, add particles

--- script.js
+++ script.js
@@ -1,45 +1,46 @@
 window.addEventListener('load', function () {
-    const canvas = this.document.getElementById('canvas1');
-    const ctx = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+  const canvas = this.document.getElementById('canvas1');
+  const ctx = canvas.getContext('2d');
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-    const image1 = this.document.getElementById('image1')
+  const image1 = document.getElementById('image1')
 
-    class Particle {
-        constructor() {
-            this.x = Math.random() * canvas.width;
-            this.y = Math.random() * canvas.height;
-            this.size = 100;
-        }
-        draw(context) {
-            context.fillRect(this.x, this.y, this.size, this.size);
-        }
+  class Particle {
+    constructor(effect) {
+      this.effect = effect;
+      this.x = Math.random() * this.effect.width;
+      this.y = Math.random() * this.effect.height;
+      this.size = 100;
+    }
+    draw(context) {
+      context.fillRect(this.x, this.y, this.size, this.size);
+    }
+  }
+
+  class Effect {
+    constructor(width, height) {
+      this.width = width;
+      this.width = height;
+      this.particlesArray = [];
+    }
+    init() {
+      for (let i = 0; i < 10; i++) {
+        this.particlesArray.push(new Particle(this));
+      }
+    }
+    draw(context) {
+      this.particlesArray.forEach(particle => particle.draw(context));
     }
 
-    class Effect {
-        constructor(width, height) {
-            this.width = width;
-            this.width = height;
-            this.particlesArray = [];
-        }
-        init() {
-            this.particlesArray.push(new Particle());
-            this.particlesArray.push(new Particle());
-            this.particlesArray.push(new Particle());
-        }
-        draw(context) {
-            this.particlesArray.forEach(particle => particle.draw(context));
-        }
+  }
 
-    }
+  const effect = new Effect(canvas.width, canvas.height)
+  effect.init();
+  effect.draw(ctx);
 
-    const effect = new Effect(canvas.width, canvas.height)
-    effect.init();
-    effect.draw(ctx);
+  function animate() {
 
-    function animate() {
-
-    }
+  }
 
 });
