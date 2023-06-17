087b0c8a78d686185a4601ba8829d04418d20937
leonzuev <loonatune@gmail.com>
2023-06-17 12:48:47 +0300

    Complete analayzing and pixeling image

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ window.addEventListener('load', function () {
   canvas.height = window.innerHeight;
 
   class Particle {
-    constructor(effect) {
+    constructor(effect, x, y, color) {
       this.effect = effect;
       this.x = Math.random() * this.effect.width;
       this.y = Math.random() * this.effect.height;
+      this.originX = Math.floor(x);
+      this.originY = Math.floor(y);
+      this.color = color;
       this.size = 10;
       this.velocityX = Math.random() * 2 - 1;
       this.velocityY = Math.random() * 2 - 1;
@@ -40,7 +43,16 @@ window.addEventListener('load', function () {
       const pixels = context.getImageData(0, 0, this.width, this.height).data;
       for (let y = 0; y < this.height; y += this.gap) {
         for (let x = 0; x < this.width; x += this.gap) {
-          
+          const index = (y * this.width + x) * 4;
+          const red = pixels[index];
+          const green = pixels[index + 1];
+          const blue = pixels[index + 2];
+          const alpha = pixels[index + 3];
+          const color = 'rgb('+ red + ',' + green + ',' + blue + ')';
+
+          if (alfa > 0) {
+            this.particlesArray.push(new Particle(this, x, y, color));
+          }
         }
       }
     }
